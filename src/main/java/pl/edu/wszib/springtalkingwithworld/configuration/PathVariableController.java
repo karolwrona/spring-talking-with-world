38eb5b9784c327cdb3e4078699c8507bed844b46
kwronski <karol.mikolaj.wronski@gmail.com>
2019-02-10 11:34:27 +0100

    10.02.2019 11:08

--- src/main/java/pl/edu/wszib/springtalkingwithworld/configuration/PathVariableController.java
+++ src/main/java/pl/edu/wszib/springtalkingwithworld/configuration/PathVariableController.java
@@ -3,16 +3,39 @@ package pl.edu.wszib.springtalkingwithworld.configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Calendar;
+
 @Controller
 @RequestMapping("/data")
 public class PathVariableController {
-    @PostMapping("/{rok123}")
-    public ResponseEntity post(@PathVariable("rok123") int rok) {
-        ResponseEntity entity = new ResponseEntity(rok+1, HttpStatus.OK);
+    @GetMapping("/{rok}/{miesiac}/{dzien}")
+    public ResponseEntity post(@PathVariable(required = true) int rok,@PathVariable(required = false) int miesiac, @PathVariable(required = false) int dzien) {
+        Calendar calendar =Calendar.getInstance();
+        calendar.set(rok, miesiac, dzien);
+        ResponseEntity entity = new ResponseEntity(calendar.getTimeInMillis(), HttpStatus.OK);
         return entity;
     }
+
+    @GetMapping("/{rok}/{miesiac}")
+    public ResponseEntity post(@PathVariable(required = true) int rok,@PathVariable(required = false) int miesiac) {
+        Calendar calendar =Calendar.getInstance();
+        calendar.set(rok, miesiac, 1);
+        ResponseEntity entity = new ResponseEntity(calendar.getTimeInMillis(), HttpStatus.OK);
+        return entity;
+    }
+
+    @GetMapping("/{rok}")
+    public ResponseEntity post(@PathVariable(required = true) int rok) {
+        Calendar calendar =Calendar.getInstance();
+        calendar.set(rok, 1, 1);
+        ResponseEntity entity = new ResponseEntity(calendar.getTimeInMillis(), HttpStatus.OK);
+        return entity;
+    }
+
+
 }
